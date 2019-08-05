8cdcb27376b441aa84ac37f3f4b21298daeebbd6
Mark Rego <rego121@gmail.com>
2019-08-05 21:46:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,3 +9,21 @@ def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
+
+def link
+ puts  "It's Dangerous To Go Alone! Take This."
+
+end
+
+def any_phrase(phrase)
+
+  phrase = "Do A Barrel Roll!"
+return phrase
+end
+
+
+ 
+  
+
+  
